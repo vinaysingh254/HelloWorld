0b8fa49cf0710797dec0486c9cc36a3e0569c008
vinaysingh254 <vinaysingh.singh254@gmail.com>
2014-06-28 00:57:07 +0530

    gfd

---
+++ src/main/java/com/yc/Factorials.java
@@ -0,0 +1,14 @@
+package com.yc;
+
+public class Factorials {
+	public static void main(String[] args) {
+		System.out.println(fact(7));
+	}
+
+	public static int fact(int n) {
+		if (n == 0)
+			return 1;
+		else
+			return n * fact(n - 1);
+	}
+}
